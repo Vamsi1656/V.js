320a1b674dee1ae21b4cdc777cf72db17ff90481
Vamsi1656 <vamsireddy1717@gmail.com>
2023-05-23 15:53:04 +0530

    okie

---
+++ Tasks/Array empty or not/one.js
@@ -0,0 +1,25 @@
+/*let a = [1,2,3,4,5]
+let b = []
+
+if(a.length>0){
+    console.log("Not empty array")
+}
+else{
+    console.log("empty array")
+}
+
+if(b.length>0){
+    console.log("not empty array")
+
+}
+else{
+    console.log("empty array")
+}*/
+
+// By using ternary operators
+
+let a = [11,2,3,4,5,6,7]
+let b = [ ]
+
+a.length>0?  console.log("not empty") : console.log("empty")
+b.length>0?  console.log("not empty") : console.log("empty")
---
+++ Tasks/Object is empty or not/object.js
@@ -0,0 +1,33 @@
+/*let emp = {
+    id:101,
+    name:"Nani",
+    sal:45000,
+}
+
+    Object.keys(emp) 
+   
+
+    if(Object.keys(emp).length>0){
+    console.log("notempty")
+   }
+else{
+        console.log("empty")
+}*/
+
+
+let emp = {
+    
+}
+
+    Object.keys(emp) 
+   
+
+    if(Object.keys(emp).length>0){
+    console.log("notempty")
+   }
+else{
+        console.log("empty")
+}
+
+
+ 
