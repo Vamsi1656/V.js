28b744ad8e7f8a8bb09f40dcc5ab157eae1acee6
Vamsi1656 <vamsireddy1717@gmail.com>
2023-05-23 12:12:34 +0530

    first commit

---
+++ Function/one.js
@@ -0,0 +1,48 @@
+/*function add(){
+    console.log("addition")
+}
+add()*/
+
+
+
+/*function add(a,b){
+    console.log(a+b)
+}
+add(10,20)
+add(30,40)*/
+
+
+
+/*function add(a,b,c=100){
+    console.log(a+b+c)
+}
+add(10,20,30)
+add(10,20)
+add(10) // number+undefined =NaN*/
+
+/*
+function cal_age(year){
+    console.log(2023-year)
+}
+cal_age(1999)*/
+
+
+/*
+function add(a,b,c=100){
+    return(a+b+c)
+}
+let x = add(10,20,30)
+console.log(x)
+
+let y = add(10,20)
+console.log(y)
+
+let z = add(10)
+console.log(z)*/
+
+
+function cal_age(year){
+    return(2023-year)
+}
+ 
+console.log(x = cal_age(1947))
---
+++ Function/syntax.js
@@ -0,0 +1,11 @@
+function functionname(){
+    console.log()
+}
+functionname() //invoking the functionname
+
+
+
+function functionname(){
+    return
+}
+let x=functionname() //invoking the functionname
