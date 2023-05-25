e0a8f52ef160d0461cbaf8ed582d0ffe4991eae1
Vamsi1656 <vamsireddy1717@gmail.com>
2023-05-25 17:18:48 +0530

    okie

---
+++ Js Events/one.js
@@ -0,0 +1,25 @@
+function cc(){
+    
+    document.getElementById("Vamsi").style.backgroundColor = "yellow"
+
+}
+
+function mouseover(){
+    
+    document.getElementById("Nani").style.backgroundColor = "Red"
+
+}
+function mouseout(){
+    
+    document.getElementById("N").style.backgroundColor = "green"
+
+}
+function dblclick(){
+    
+    document.getElementById("A").style.backgroundColor = "orange"
+
+}
+
+function Focus() {
+    document.getElementById('focusid').style.backgroundColor = "Violet"
+}
