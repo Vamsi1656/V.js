02626211bbc9f0e0e21c789ceb43733b0854af52
Vamsi1656 <vamsireddy1717@gmail.com>
2023-05-25 17:34:50 +0530

    okie

---
+++ Function/fat arrow function/one.js
@@ -0,0 +1,14 @@
+// function add(a,b){
+//     console.log(a+b)
+// }
+// add(10,20)
+
+
+// let add=(a,b)=>{console.log(a+b)}
+// add(10,20)
+
+// fat arrow function is used for alternate syntax for function.
+
+
+let  emp=(id,Name)=>{console.log(id,Name)}
+emp(101,"Nani")
---
+++ Tasks/remove duplicates from array/one.js
@@ -0,0 +1,9 @@
+let arr = ["apple", "mango", "apple",
+          "orange", "mango", "mango"];
+ 
+function removeDuplicates(arr) {
+    return [...new Set(arr)];
+}
+ 
+console.log(removeDuplicates(arr));
+
