27c7149986036a271eaab9fbcc028f32858e9585
Vamsi1656 <vamsireddy1717@gmail.com>
2023-05-26 19:51:26 +0530

    okie

---
+++ Function/fat arrow function/1.js
@@ -0,0 +1,29 @@
+// function add(a,b){
+//     console.log(a+b)
+// }
+// add(10,20)
+
+
+// let add = (a,b)=>{
+//      console.log(a+b)
+// }
+// add(100,300)
+
+
+//  function add(a,b){
+//     return(a+b)
+//  }
+//  let x= add(33,99)
+//  console.log(x)
+
+
+// let add= (a,b)=>{
+//     return(a+b)
+// }
+// let x=add(70,90)
+// console.log(x)
+
+
+
+
+
