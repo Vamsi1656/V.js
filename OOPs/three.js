167dd27f1b0a907e9908f0864780e4bd593c1ac4
Vamsi1656 <vamsireddy1717@gmail.com>
2023-06-01 16:21:44 +0530

    okie

---
+++ OOPs/three.js
@@ -0,0 +1,13 @@
+class Account{
+    
+    
+}
+class Savingamount extends Account{
+    constructor(id,name,loc,amount){
+
+    }
+}
+let sa1=new Account(101,"Vamsi","Guntur",55000)
+let sa2=new Account()
+console.log(sa1)
+console.log(sa2)
---
+++ OOPs/two.js
@@ -0,0 +1,9 @@
+class Parent{
+    Amount ='100cr'
+}
+class child extends Parent{
+}
+let c1=new child()
+let c2=new child()
+console.log(c1.Amount)
+console.log(c2.Amount)
