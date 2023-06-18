0561e55f4ba804320194653e8d00123cbf4fc64b
Vamsi1656 <vamsireddy1717@gmail.com>
2023-06-18 18:53:49 +0530

    sets

---
+++ ES6/sets.js
@@ -0,0 +1,11 @@
+let letters=new Set()
+
+letters.add("a")
+letters.add("b")
+letters.add("c")
+letters.add("d")
+console.log(letters.size)
+
+// A JavaScript Set is a collection of unique values.
+// Each value can only occur once in a Set.
+// A Set can hold any value of any data type.
