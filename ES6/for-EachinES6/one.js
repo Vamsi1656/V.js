db212b1b6860bb52e135170892aa4a50738a6f2d
Vamsi1656 <vamsireddy1717@gmail.com>
2023-06-30 15:01:10 +0530

    foreach

---
+++ ES6/for-EachinES6/one.js
@@ -0,0 +1,15 @@
+let ids=[101,102,103,104,105]
+
+ids.forEach((id)=>{console.log(id)})
+
+//for-each method is used to iterate and execute.
+
+
+let enames=["Nani","Virat","Pallavi","Vamsi"]
+
+ let a=enames.forEach((ename)=>{
+return ename;
+})
+console.log(a); // output is undefined
+//Because it can only iterate and cannot return any value,it is undefined
+
