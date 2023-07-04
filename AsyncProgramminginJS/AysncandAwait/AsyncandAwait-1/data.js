8b79c9df7af1365f376c873d3ddbee04bc3981e2
Vamsi1656 <vamsireddy1717@gmail.com>
2023-07-04 10:18:09 +0530

    AyncandAwait

---
+++ AsyncProgramminginJS/AysncandAwait/AsyncandAwait-1/data.js
@@ -0,0 +1,33 @@
+let employees=[{id:101,name:"Nani",Loc:"Hyd"},
+              {id:102,name:"Virat",Loc:"Mumbai"},
+              {id:103,name:"Pallavi",Loc:"Kerala"}]
+        
+let createEmployees=(employee)=>{
+          return new Promise((resolve,reject)=>{
+          setTimeout(() => {
+            let flag = true;
+            flag ? resolve("Data Inserted") : reject("Not Inserted")
+            employees.push(employee)
+        
+          }, 4000)
+        })
+}
+let getEmployees = () => {
+    setTimeout(() => {
+        let rows = ""
+        for (emp of employees) {
+            rows = rows + `<tr>
+              <td>${emp.id}</td>            
+              <td>${emp.name}</td>            
+              <td>${emp.Loc}</td>            
+            </tr>`
+        }
+        document.getElementById('abc').innerHTML = rows
+    }, 1000)
+}
+
+let exec=async()=>{
+     await createEmployees({id:104,name:"Vamsi",Loc:"Guntur"})
+     getEmployees()
+}
+exec()             
