378c8d962e59d765add890a537b99e4472a9e45f
Vamsi1656 <vamsireddy1717@gmail.com>
2023-07-05 10:44:38 +0530

    classes

--- array methods/slice.js
+++ array methods/slice.js
@@ -1,6 +1,6 @@
 let numbers=[10,20,30,40,50]
 console.log(numbers)
-numbers.slice(0,5)
+numbers.slice(1,3)
 console.log(numbers)
 
-//This method returns a new array containing a portion of the original array, based on the start and end index provided as arguments
+// Array.slice() returns selected array elements as a new array:
